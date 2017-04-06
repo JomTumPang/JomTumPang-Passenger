bf34c3455cd33d741ac921d8613de243b2408fd2
Venkat <balu.venky@gmail.com>
2017-04-06 06:27:18 +0530

    Initial Commit

---
+++ JTPPassenger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JTPPassenger
+//
+//  Created by Venkat on 06/04/17.
+//  Copyright © 2017 JomTumpang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JTPPassenger/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JTPPassenger
+//
+//  Created by Venkat on 06/04/17.
+//  Copyright © 2017 JomTumpang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
